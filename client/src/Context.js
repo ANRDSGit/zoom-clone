520f0af755562ec606c460644a085a5a96620148
A. Nipun Rasanajan de Silva <anrds.git@gmail.com>
2024-09-15 02:25:50 +0530

    Update proxy and server configuration

--- client/src/Context.js
+++ client/src/Context.js
@@ -4,7 +4,8 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000');
+
+const socket = io('https://zoom-clone-server-iota.vercel.app/');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
